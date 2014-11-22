634decf79e511a324744e8bb163f0bb43fef0483
tzwzero@163.com <tzwzero@163.com>
2014-11-22 16:43:51 +0800

    fork diamond分支到自己的repo，修改了CONSTANT

---
+++ diamond-client/src/test/java/com/taobao/diamond/client/Test.java
@@ -0,0 +1,33 @@
+//package com.taobao.diamond.client;
+//
+//import com.taobao.diamond.manager.ManagerListener;
+//import com.taobao.diamond.manager.impl.DefaultDiamondManager;
+//
+//import java.util.concurrent.Executor;
+//
+///**
+// * Created by yuanfei on 14/11/22.
+// */
+//public class Test {
+//
+//    public static void main(String[] args) {
+//        ManagerListener testManagerListener = new TestDiamond();
+//        DefaultDiamondManager defaultDiamondManager = new DefaultDiamondManager("aaa",testManagerListener);
+//    }
+//    static class TestDiamond implements ManagerListener{
+//
+//        @Override
+//        public Executor getExecutor() {
+//            return null;
+//        }
+//
+//        @Override
+//        public void receiveConfigInfo(String configInfo) {
+//
+//            if(null != configInfo && !configInfo.equals("")){
+//                System.out.println(configInfo);
+//            }
+//        }
+//    }
+//
+//}
