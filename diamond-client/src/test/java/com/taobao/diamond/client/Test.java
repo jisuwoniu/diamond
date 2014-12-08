1f61ffedbfb061fafbce9e1713dde1e1328f2113
tzwzero@163.com <tzwzero@163.com>
2014-12-08 14:45:04 +0800

    modified demo

--- diamond-client/src/test/java/com/taobao/diamond/client/Test.java
+++ diamond-client/src/test/java/com/taobao/diamond/client/Test.java
@@ -1,33 +1,39 @@
-//package com.taobao.diamond.client;
-//
-//import com.taobao.diamond.manager.ManagerListener;
-//import com.taobao.diamond.manager.impl.DefaultDiamondManager;
-//
-//import java.util.concurrent.Executor;
-//
-///**
-//* Created by yuanfei on 14/11/22.
-//*/
-//public class Test {
-//
-//    public static void main(String[] args) {
-//        ManagerListener testManagerListener = new TestDiamond();
-//        DefaultDiamondManager defaultDiamondManager = new DefaultDiamondManager("aaa",testManagerListener);
-//    }
-//    static class TestDiamond implements ManagerListener{
-//
-//        @Override
-//        public Executor getExecutor() {
-//            return null;
-//        }
-//
-//        @Override
-//        public void receiveConfigInfo(String configInfo) {
-//
-//            if(null != configInfo && !configInfo.equals("")){
-//                System.out.println(configInfo);
-//            }
-//        }
-//    }
-//
-//}
+package com.taobao.diamond.client;
+
+import com.taobao.diamond.manager.ManagerListener;
+import com.taobao.diamond.manager.impl.DefaultDiamondManager;
+
+import java.util.concurrent.Executor;
+
+/**
+* Created by yuanfei on 14/11/22.
+*/
+public class Test {
+
+    public static void main(String[] args) {
+        ManagerListener testManagerListener = new TestDiamond();
+        DefaultDiamondManager defaultDiamondManager = new DefaultDiamondManager("com.sfebiz.diamond.test",testManagerListener);
+        String configInfo = defaultDiamondManager.getAvailableConfigureInfomation(1000);
+        parseConfigInfo(configInfo);
+
+    }
+    static class TestDiamond implements ManagerListener{
+
+        @Override
+        public Executor getExecutor() {
+            return null;
+        }
+
+        @Override
+        public void receiveConfigInfo(String configInfo) {
+            parseConfigInfo(configInfo);
+        }
+    }
+    public static void parseConfigInfo(String configInfo){
+
+        if(null != configInfo && !(configInfo.trim()).equals("")){
+            System.out.println(configInfo);
+        }
+    }
+
+}
